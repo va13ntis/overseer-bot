cb9ae27fd1acd57ec36dcc92613c945a26e183f9
Val Stashevski <valentin.statshevski@gmail.com>
2025-03-28 13:25:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+import logging
+import os
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BAN_KEYWORDS = ["18+"]
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+async def check_new_members(update: Update, context):
+    chat_id = update.message.chat_id  # Get the group ID dynamically
+    for member in update.message.new_chat_members:
+        for keyword in BAN_KEYWORDS:
+            if member.name and keyword.lower() in member.name.lower():
+                await ban_message(context, update, member, chat_id)
+            elif member.full_name and keyword.lower() in member.full_name.lower():
+                await ban_message(context, update, member, chat_id)
+            elif member.first_name and keyword.lower() in member.first_name.lower():
+                await ban_message(context, update, member, chat_id)
+            elif member.last_name and keyword.lower() in member.last_name.lower():
+                await ban_message(context, update, member, chat_id)
+
+
+async def start(update: Update, context):
+    await update.message.reply_text("Bot is running and monitoring new members.")
+
+
+async def ban_message(context, update, member, chat_id):
+    await context.bot.ban_chat_member(chat_id, member.id)
+    await update.message.reply_text(f"Banned {member.full_name} for violating rules.")
+    logger.info(f"Banned {member.username} ({member.id}) in group {chat_id}")
+    return
+
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, check_new_members))
+
+    logger.info("Bot started...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
