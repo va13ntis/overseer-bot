9f89623c83e2ee92fd9ae9fecb213ec5ac0288b5
Val Stashevski <valentin.statshevski@gmail.com>
2025-06-14 00:58:02 +0300

    Cover more cases - Add check public profile - Change polling interval

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 import logging
 import os
-import random
 import time
+from string import Template
 
+import requests
 from telegram import Update
-from telegram.constants import ChatAction
-from telegram.ext import CommandHandler, MessageHandler, filters, ApplicationBuilder, CallbackContext
+from telegram.ext import MessageHandler, filters, ApplicationBuilder, CallbackContext
 
-
-BOT_NAME = "@va13ntis_bot"
+POLL_INTERVAL = os.getenv("BOT_POLL_INTERVAL", 10.0)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BAN_KEYWORDS = []
-
+PUBLIC_PROFILE_TEMPLATE = Template("https://t.me/$username")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,32 +21,6 @@ def get_keywords():
             BAN_KEYWORDS.append(keyword.rstrip("\r\n"))
 
     logger.info(f"Ban keywords: {BAN_KEYWORDS}")
-
-
-async def handle_messages(update: Update, context: CallbackContext):
-    """Let the bot react naturally to group messages"""
-    message = update.message.text.lower()
-    chat_id = update.message.chat_id
-    user_name = update.effective_user.first_name
-
-    # Simulate "thinking..."
-    await context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
-    time.sleep(random.uniform(1, 3))
-
-    # Bot responds if someone mentions it
-    if BOT_NAME in message:
-        responses = [
-            f"А, {user_name}, к вашим услугам. ☕",
-            "Чудненько, сделаем сию секунду! 🎩",
-            "Кто там?",
-            "Разрешите вставить ремарку..."
-        ]
-
-        await update.message.reply_text(random.choice(responses))
-
-    # React if someone mentions tea 🍵
-    elif "привет" in message:
-        await update.message.reply_text("И вам не хворать...")
 
 
 async def handle_new_members(update: Update, context: CallbackContext):
@@ -65,7 +38,12 @@ async def handle_new_members(update: Update, context: CallbackContext):
                 return
             elif member.last_name and keyword.lower() in member.last_name.lower():
                 await ban_message(context, update, member, chat_id)
+                return
 
+        # Fallback: check public profile
+        if not is_allowed_public_profile(member.username):
+            await ban_message(context, update, member, chat_id)
+            return
 
 async def start(update: Update, context):
     await update.message.reply_text("Bot is running and monitoring new members.")
@@ -78,6 +56,25 @@ async def ban_message(context, update, member, chat_id):
     return
 
 
+def is_allowed_public_profile(username):
+    is_allowed = True
+
+    try:
+        response = requests.get(PUBLIC_PROFILE_TEMPLATE.substitute(username=username))
+
+        if response.status_code == 200 and response.text and is_ban_word(response.text):
+            is_allowed = False
+    except Exception as e:
+        logger.error(e)
+        is_allowed = False
+
+    return is_allowed
+
+
+def is_ban_word(content):
+    return any(substring.casefold() in content.casefold() for substring in BAN_KEYWORDS)
+
+
 def main():
     get_keywords()
 
@@ -86,14 +83,11 @@ def main():
     # Ban unwanted users when they join
     app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, handle_new_members))
 
-    # Listen for chat messages and engage in conversations
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_messages))
-
     logger.info("Bot started...")
 
     while True:
         try:
-            app.run_polling()
+            app.run_polling(poll_interval=POLL_INTERVAL)
         except Exception as e:
             print(f"Bot crashed: {e}")
             time.sleep(5)
